28b530b82947fe2defe2dd4f002c706cf98b68f8
SofiaK <skamage9193@sdsu.edu>
2025-10-28 17:05:26 -0700

    Corrected push() and pop() methods and added comments.

--- heap.h
+++ heap.h
@@ -16,20 +16,55 @@ struct MinHeap {
 
     void push(int idx, int weightArr[]) {
         // TODO: insert index at end of heap, restore order using upheap()
+        // Check if index is a valid integer
+        if (size <= idx || idx < 0) {
+            cout<<"Invalid index."<<endl;
+            return;
+        }
 
-        data[size-1] = weightArr[idx];
+        // Check if heap is full
+        if (size >= 64) {
+            cout<<"Heap is full."<<endl;
+            return;
+        }
+
+        // Insert index at end of heap
+        data[size] = weightArr[idx];
+
+        // Increment size of heap
         size++;
-        upheap(data[size], data);
+
+        // Restore order of heap calling upheap()
+        upheap(data[size - 1], weightArr);
     }
 
     int pop(int weightArr[]) {
         // TODO: remove and return smallest index
         // Replace root with last element, then call downheap()
-        int temp = data[size-1];
-        size--;
-        downheap(data[size], weightArr);
 
-        return temp;
+        // Check if heap is empty
+        if (size == 0) {
+            cout<<"Heap is empty."<<endl;
+            return 0;
+        }
+
+        // Assign root to temp variable
+        int min = data[0];
+
+        // Replace root with last element
+        data[0] = data[size - 1];
+
+        // Decrement size of heap
+        size--;
+
+        // Check if heap is now empty and return smallest index
+        if (size == 0) {
+            return min;
+        }
+
+        // Restore order of heap calling downheap() and return smallest index
+        downheap(data[size], weightArr);
+        return min;
     }
 
     void upheap(int pos, int weightArr[]) {
